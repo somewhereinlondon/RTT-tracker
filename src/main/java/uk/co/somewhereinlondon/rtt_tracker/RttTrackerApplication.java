341bcf8ac826f7e60a13b07c641f74f6fd587a64
somewhereinlondon <Robel.rh123@gmail.com>
2026-09-05 17:09:56 +0100

    Scaffold Spring Boot project

---
+++ src/main/java/uk/co/somewhereinlondon/rtt_tracker/RttTrackerApplication.java
@@ -0,0 +1,13 @@
+package uk.co.somewhereinlondon.rtt_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RttTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RttTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/somewhereinlondon/rtt_tracker/RttTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.somewhereinlondon.rtt_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RttTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
